87db23c26e5616687e834be078fc82a8058f76c5
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-04-19 04:28:05 +0530

    fix: Adjust layout properties in DarkSidebar for improved sticky behavior and overflow handling

--- src/components/DarkSidebar.tsx
+++ src/components/DarkSidebar.tsx
@@ -38,17 +38,17 @@ export function DarkSidebar({ children }: Props) {
   return (
     <div
       className={cn(
-        "mx-auto flex w-full flex-1 flex-col overflow-auto rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
+        "sticky flex w-full flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
         "h-screen"
       )}
     >
       <Sidebar open={open} setOpen={setOpen} >
         <SidebarBody className="justify-between gap-10">
-          <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+          <div className="sticky flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
             {/* <Logo /> */}
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
-                <SidebarLink key={idx} link={link} />
+                <SidebarLink key={idx} link={link} className="" />
               ))}
             </div>
           </div>
@@ -65,10 +65,8 @@ export function DarkSidebar({ children }: Props) {
       </Sidebar>
 
       {/* Child content here */}
-      <div className="flex flex-1">
-        <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900">
-          {children}
-        </div>
+      <div className="flex flex-1 overflow-auto">
+        <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900">{children}</div>
       </div>
     </div>
   );
