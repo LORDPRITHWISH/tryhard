0ffbbaaa28b15fb68feee5dfc15e0f1a10b7557a
SandipM03 <sandipmandal816708@gmail.com>
2025-04-19 16:52:58 +0530

    fix:feature card

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   },
+  images: {
+    domains: ['ik.imagekit.io'],
+  },
 };
 
 module.exports = nextConfig; 
