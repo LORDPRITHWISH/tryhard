ed0d9e69d4fbcb78676273642d2c37ddce035a0e
SandipM03 <sandipmandal816708@gmail.com>
2025-04-19 11:25:22 +0530

    fix: Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,47 +14,53 @@ import {
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-    <div className="flex items-center justify-center h-[7rem] w-full">
-    {/* <NavbarPage/> */}
-    <FloatingDock 
-      items={[
-        { title: "Home",
-          icon: (
-            <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-          ),
-          href: "#", },
-        { title: "Components",
-          icon: (
-            <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-          ),
-          href: "#", },
-        { title: "Changelog",
-          icon: (
-            <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-          ),
-          href: "#", },
-        { title: "Twitter",
-          icon: (
-            <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-          ),
-          href: "#", },
-        { title: "GitHub",
-          icon: (
-            <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-          ),
-          href: "#", },
-        
-      ]}
-     />
-    </div>
-    <div className='page-transition'>
-    <HeroPage/>
-      
-      <FeaturesSection/>
-      <PricingPage/>
-    </div>
-    <FintreeFooter/>
+    <main className="min-h-screen flex flex-col">
+      {/* Navigation Section */}
+      <div className="fixed top-0 left-0 right-0 z-50 bg-black/50 backdrop-blur-sm">
+        <div className="flex items-center justify-center h-20 w-full">
+          <FloatingDock 
+            items={[
+              { 
+                title: "Home",
+                icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+                href: "#" 
+              },
+              { 
+                title: "Components",
+                icon: <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+                href: "#" 
+              },
+              { 
+                title: "Changelog",
+                icon: <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+                href: "#" 
+              },
+              { 
+                title: "Twitter",
+                icon: <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+                href: "#" 
+              },
+              { 
+                title: "GitHub",
+                icon: <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+                href: "#" 
+              },
+            ]}
+          />
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 pt-20">
+        <div className="space-y-20">
+          <HeroPage />
+          <FeaturesSection />
+          <PricingPage />
+        </div>
+      </div>
+
+      {/* Footer */}
+      <FintreeFooter />
     </main>
   );
 }
