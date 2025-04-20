3a7f6d8e4c82d878be08628c8a38e5a46501155a
SandipM03 <sandipmandal816708@gmail.com>
2025-04-20 09:06:37 +0530

    fix:page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,39 +17,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-black to-gray-900">
      
-      <div className="fixed top-0 left-0 right-0 z-50 backdrop-blur-sm">
-        <div className="flex items-center justify-center h-20 w-full">
-          <FloatingDock 
-            items={[
-              {
-                title: "Home",
-                icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-                href: "#"
-              },
-              {
-                title: "Components",
-                icon: <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-                href: "#"
-              },
-              {
-                title: "Changelog",
-                icon: <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-                href: "#"
-              },
-              {
-                title: "Twitter",
-                icon: <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-                href: "#"
-              },
-              {
-                title: "GitHub",
-                icon: <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-                href: "#"
-              },
-            ]}
-          />
-        </div>
-      </div>
+     
 
       
       <div className="flex-1 pt-20">
